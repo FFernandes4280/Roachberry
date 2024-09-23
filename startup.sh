aaaa03b5a3021b6c14e3783cdfff2ff344293a5e
FFernandes4280 <d2022002088@unifei.edu.br>
2024-09-23 19:07:53 -0300

    startup updated

--- startup.sh
+++ startup.sh
@@ -2,18 +2,11 @@
 
 cd kafka
 echo "Starting Zookeeper..."
-#nohup bin/zookeeper-server-start.sh config/zookeeper.properties > zookeeper.log 2>&1 &
-
-# Give Zookeeper a few seconds to start
-#sleep 5
-
-# Start Kafka server in the background with nohup and log the output
+nohup bin/zookeeper-server-start.sh config/zookeeper.properties > zookeeper.log 2>&1 &
+sleep 5
 echo "Starting Kafka server..."
-#nohup bin/kafka-server-start.sh config/server.properties > kafka.log 2>&1 &
-
-#sleep 5
-
-#Creating topicos
+nohup bin/kafka-server-start.sh config/server.properties > kafka.log 2>&1 &
+sleep 5
 echo "Creating topics..."
 bin/kafka-topics.sh --create --zookeeper localhost:2181 --replication-factor 1 --partitions 2 --topic response-topic
 bin/kafka-topics.sh --create --zookeeper localhost:2181 --replication-factor 1 --partitions 2 --topic request-topic
@@ -24,9 +17,6 @@ bin/kafka-topics.sh --create --zookeeper localhost:2181 --replication-factor 1 -
 bin/kafka-topics.sh --create --zookeeper localhost:2181 --replication-factor 1 --partitions 1 --topic request-concatena-string-topic
 bin/kafka-topics.sh --create --zookeeper localhost:2181 --replication-factor 1 --partitions 1 --topic request-salva-arquivo-topic
 bin/kafka-topics.sh --create --zookeeper localhost:2181 --replication-factor 1 --partitions 1 --topic request-calcula-funcao-topic
-
 cd ..
-
 cd roachberry
-
 echo "Please run the topic router or add the command to this script..."
