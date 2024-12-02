46290bcf78f17b3953008a36319762e7bb2bb086
FFernandes4280 <filipecfernandes2004@hotmail.com>
2024-12-02 00:15:01 -0300

    Testar rasp

--- startup.sh
+++ startup.sh
@@ -8,15 +8,13 @@ echo "Starting Kafka server..."
 nohup bin/kafka-server-start.sh config/server.properties > kafka.log 2>&1 &
 sleep 5
 echo "Creating topics..."
-bin/kafka-topics.sh --create --topic request-topic --bootstrap-server localhost:9092
-bin/kafka-topics.sh --create --topic response-topic --bootstrap-server localhost:9092
-
-bin/kafka-topics.sh --create --topic request-concatena-string-topic --bootstrap-server localhost:9092
-bin/kafka-topics.sh --create --topic response-concatena-string-topic --bootstrap-server localhost:9092
-bin/kafka-topics.sh --create --topic request-salva-arquivo-topic --bootstrap-server localhost:9092
-bin/kafka-topics.sh --create --topic response-salva-arquivo-topic --bootstrap-server localhost:9092
-bin/kafka-topics.sh --create --topic request-calcula-funcao-topic --bootstrap-server localhost:9092
-bin/kafka-topics.sh --create --topic response-calcula-funcao-topic --bootstrap-server localhost:9092
+#Kafka Cluster
+bin/kafka-topics.sh --create --topic source-db-topic --bootstrap-server localhost:9092
+bin/kafka-topics.sh --create --topic source-ligh-topic --bootstrap-server localhost:9092
+bin/kafka-topics.sh --create --topic qrcode-content-topic --bootstrap-server localhost:9092
+bin/kafka-topics.sh --create --topic save-data-db-topic --bootstrap-server localhost:9092
+bin/kafka-topics.sh --create --topic light-control-topic --bootstrap-server localhost:9092
+#Kafka Connect Cluster
 
 cd ..
 cd roachberry
